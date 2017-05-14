ee111533ad2a00a87c0e97f9b59df069bd4cfa8f
stars <1967282902@qq.com>
2017-05-14 21:21:41 +0800

    chages

---
+++ Task02/js/main.js
@@ -0,0 +1,9 @@
+function hidemouse(li){
+	var abcd=li.getElementsByTagName("ul")[0];
+	abcd.style.display="none";
+}
+
+function showmouse(li){
+	var abcd=li.getElementsByTagName("ul")[0];
+	abcd.style.display="block";
+}
